8fea03606ac72f89ba345c6d639e873e46d5e8a6
Ramil Aminev <Ramil_Aminev@epam.com>
2021-12-17 20:04:48 +0400

    Init Commit

---
+++ src/main/java/com/starbun/cardengine/CardEngineApplication.java
@@ -0,0 +1,13 @@
+package com.starbun.cardengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardEngineApplication.class, args);
+	}
+
+}
